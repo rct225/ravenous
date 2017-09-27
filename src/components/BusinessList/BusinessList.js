4d9c700871fe75df71e81b4dca54a347e3aff7c6
Robert Tuck <robtuck@gmail.com>
2017-09-26 23:42:31 -0400

    changed Business list to include business.id as key for Business Component objects

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -7,8 +7,8 @@ class BusinessList extends React.Component {
     return (
       <div className="BusinessList">
       {
-        this.props.businesses.map((business, index) => {
-          return <Business key={index} business={business} />;
+        this.props.businesses.map(business => {
+          return <Business key={business.id} business={business} />;
         })
       }
       </div>
