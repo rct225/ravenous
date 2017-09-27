ba7dfb6c44e0dffde78cd83c0379f1ad0f99d258
Robert Tuck <robtuck@gmail.com>
2017-09-27 00:16:09 -0400

    updated App.js to get data from Yelp utility.  various bugfixes for typos in Yelp.js.  fixed category property to point to correct item in Yelp data.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,43 @@ import React, { Component } from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
+// const business = {
+//   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// };
+//
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ];
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log("Searching Yelp with " + term + ", " + location + ", " + sortBy)
+    // console.log("Searching Yelp with " + term + ", " + location + ", " + sortBy)
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({ businesses: businesses })
+    });
   }
 
   render() {
@@ -35,7 +47,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp.bind(this)}/>
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,6 @@
 const clientId = 'WgZc1KEr7nxW0-XUXWKdDg';
 const secret = '8sTGe8Cgaq33OxG782IhyEdpQxogCpsEeBBOvb9mjAOZqy32vmouuv3nAs2z3bZv';
-const accessToken = '';
+let accessToken = '';
 
 const Yelp = {
   getAccessToken() {
@@ -9,16 +9,16 @@ const Yelp = {
         resolve(accessToken);
       });
     }
-    return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
     { method: 'POST'}).then( response => {
       return response.json();
-    }).then( jsonResponse = > {
+    }).then( jsonResponse => {
       accessToken = jsonResponse.access_token;
     });
   },
   search(term, location, sortBy) {
     return Yelp.getAccessToken().then( () => {
-      return fetch(`https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sort_by}`,
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       { headers: { Authorization: `Bearer ${accessToken}`}}).then (response => {
         return response.json();
       }).then( jsonResponse => {
@@ -32,7 +32,7 @@ const Yelp = {
                 city: business.location.city,
                 state: business.location.state,
                 zipCode: business.location.zip_code,
-                category: business.categories,
+                category: business.categories[0].title,
                 rating: business.rating,
                 reviewCount: business.review_count
               }
